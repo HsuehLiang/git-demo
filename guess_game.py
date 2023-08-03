e63a091e8d5e342f0f5ccc8432944e23065d8244
Liang林學亮 <liang.lin@car-plus.com.tw>
2023-08-03 12:03:55 +0800

    修改顯示提區及提示語

--- guess_game.py
+++ guess_game.py
@@ -9,7 +9,7 @@ count = 0
 start, end = 1, 50
 
 for i in range(5):
-    y  = eval(input('輸入(1 ~ 50)之間的一個數字：'))
+    y  = eval(input(f'輸入({start} ~ {end})之間的一個數字：'))
     if x == y:
         win = True
         break
@@ -28,4 +28,4 @@ for i in range(5):
 if win:
     print(f'恭喜過關! 共猜了{count+1}次')
 else:
-    print(f'沒猜中，遊戲結束! ans：{x}')
+    print(f'沒猜中，遊戲失敗! ans：{x}')
